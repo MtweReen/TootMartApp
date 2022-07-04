c889f3d22bf478a187c3c6b72a4d13dfaee759d5
nermeen841 <63563785+nermeen841@users.noreply.github.com>
2022-07-04 14:05:35 +0200

    sub category

--- lib/features/subCategory/componnent/body.dart
+++ lib/features/subCategory/componnent/body.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:toot_mart/features/subCategory/componnent/products.dart';
 
+import '../../../core/constants/colors.dart';
 import '../../../core/constants/constants.dart';
 import '../../../core/utiles/size_config.dart';
+import '../../../core/widgets/space_widget.dart';
 
 class SubCategoryBody extends StatefulWidget {
   const SubCategoryBody({Key? key}) : super(key: key);
@@ -16,9 +19,9 @@ class _SubCategoryBodyState extends State<SubCategoryBody> {
     SizeConfig().init(context);
     return SingleChildScrollView(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Stack(
-            alignment: AlignmentDirectional.center,
             children: [
               SizedBox(
                 width: double.infinity,
@@ -28,37 +31,31 @@ class _SubCategoryBodyState extends State<SubCategoryBody> {
                   fit: BoxFit.cover,
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: SizeConfig.screenWidth! * 0.03,
-                  vertical: SizeConfig.screenHeight! * 0.03,
-                ),
-                child: Container(
-                  width: double.infinity,
-                  height: SizeConfig.screenHeight! * 0.3,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      end: Alignment.topCenter,
-                      begin: Alignment.bottomCenter,
-                      colors: [
-                        Colors.black,
-                        Colors.transparent,
-                        Colors.transparent,
-                      ],
-                    ),
+              Container(
+                width: double.infinity,
+                height: SizeConfig.screenHeight! * 0.3,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    end: Alignment.topCenter,
+                    begin: Alignment.bottomCenter,
+                    colors: [
+                      Colors.black,
+                      Colors.transparent,
+                      Colors.transparent,
+                    ],
                   ),
-                  child: Center(
-                    child: Text(
-                      "ابهريهم بفازتك ",
-                      textAlign: TextAlign.center,
-                      style: headingStyle.copyWith(
-                          fontSize: SizeConfig.screenWidth! * 0.05,
-                          foreground: Paint()
-                            ..style = PaintingStyle.stroke
-                            // ..strokeWidth = 1
-                            ..color = Colors.white,
-                          fontWeight: FontWeight.w700),
-                    ),
+                ),
+                child: Center(
+                  child: Text(
+                    "ابهريهم بفازتك",
+                    textAlign: TextAlign.center,
+                    style: headingStyle.copyWith(
+                        fontSize: SizeConfig.screenWidth! * 0.05,
+                        foreground: Paint()
+                          ..style = PaintingStyle.stroke
+                          // ..strokeWidth = 1
+                          ..color = Colors.white,
+                        fontWeight: FontWeight.w700),
                   ),
                 ),
               ),
@@ -68,7 +65,28 @@ class _SubCategoryBodyState extends State<SubCategoryBody> {
             padding: EdgeInsets.symmetric(
                 horizontal: SizeConfig.screenWidth! * 0.03,
                 vertical: SizeConfig.screenHeight! * 0.03),
-            child: Column(),
+            child: ListView.separated(
+                shrinkWrap: true,
+                primary: false,
+                itemBuilder: (context, index) => Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const VerticalSpace(value: 2),
+                        Text(
+                          translateString("تشكيلة فازات", "تشكيلة فازات"),
+                          style: headingStyle.copyWith(
+                              fontWeight: FontWeight.w700,
+                              color: colordeepGrey,
+                              fontSize: SizeConfig.screenWidth! * 0.05),
+                        ),
+                        const VerticalSpace(value: 2),
+                        const SubCategoryProducts(),
+                      ],
+                    ),
+                separatorBuilder: (context, index) =>
+                    const VerticalSpace(value: 2),
+                itemCount: 10),
           ),
         ],
       ),
---
+++ lib/features/subCategory/componnent/products.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:toot_mart/core/widgets/space_widget.dart';
+
+import '../../../core/utiles/size_config.dart';
+
+class SubCategoryProducts extends StatefulWidget {
+  const SubCategoryProducts({Key? key}) : super(key: key);
+
+  @override
+  State<SubCategoryProducts> createState() => _SubCategoryProductsState();
+}
+
+class _SubCategoryProductsState extends State<SubCategoryProducts> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return SizedBox(
+      height: SizeConfig.screenHeight! * 0.3,
+      child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: StaggeredGrid.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+            children: [
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: SizedBox(
+                  // height: SizeConfig.screenHeight! * 0.3,
+                  child: ClipRRect(
+                    child: Image.network(
+                      "https://img.freepik.com/free-vector/print_680502-3.jpg?size=626&ext=jpg&ga=GA1.2.756222056.1654592344",
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: SizedBox(
+                  // height: SizeConfig.screenHeight! * 0.3,
+                  child: ClipRRect(
+                    child: Image.network(
+                      "https://img.freepik.com/free-vector/print_680502-3.jpg?size=626&ext=jpg&ga=GA1.2.756222056.1654592344",
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: SizedBox(
+                  // height: SizeConfig.screenHeight! * 0.3,
+                  child: ClipRRect(
+                    child: Image.network(
+                      "https://img.freepik.com/free-vector/print_680502-3.jpg?size=626&ext=jpg&ga=GA1.2.756222056.1654592344",
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: SizedBox(
+                  // height: SizeConfig.screenHeight! * 0.3,
+                  child: ClipRRect(
+                    child: Image.network(
+                      "https://img.freepik.com/free-vector/print_680502-3.jpg?size=626&ext=jpg&ga=GA1.2.756222056.1654592344",
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 2,
+                mainAxisCellCount: 1.5,
+                child: SizedBox(
+                  // height: SizeConfig.screenHeight! * 0.3,
+                  child: ClipRRect(
+                    child: Image.network(
+                      "https://img.freepik.com/free-vector/print_680502-3.jpg?size=626&ext=jpg&ga=GA1.2.756222056.1654592344",
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
