7d6d7f4be60cd6fb424bdf8edfb51e838986fe7d
Anas <anas.ss@yahoo.com>
2022-08-27 14:54:33 +0200

    ios update

--- lib/business_logic/app_cubit/app_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'app_cubit.dart';
-
-@immutable
-abstract class AppState {}
-
-class AppInitial extends AppState {}
-class CountrySelection extends AppState {}
-class LanguageSelection extends AppState {}
